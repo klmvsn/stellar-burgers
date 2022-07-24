598f4bae57b01bcb9d653af2f446519c961b5a3b
Sofia Klimova <klimovasonya@gmail.com>
2022-07-24 13:08:00 +0300

    fix: improved UX, update README

--- react-burger/src/services/actions/burger-constructor.js
+++ react-burger/src/services/actions/burger-constructor.js
@@ -2,6 +2,7 @@ export const ADD_BUNS = 'ADD_BUNS';
 export const ADD_FILLING = 'ADD_FILLING';
 export const REMOVE_FILLING = 'REMOVE_FILLING';
 export const SWAP_FILLING = 'SWAP_FILLING';
+export const RESET_CONSTRUCTOR = 'RESET_CONSTRUCTOR';
 
 export const addFilling = (item, uniqueId) => ({ type: ADD_FILLING, payload: { ...item, uniqueId } });
 export const addBun = (item, uniqueId) => ({ type: ADD_BUNS , payload: { ...item, uniqueId }});
--- react-burger/src/services/actions/order-details.js
+++ react-burger/src/services/actions/order-details.js
@@ -1,16 +1,20 @@
 import { postOrder } from '../../utils/api';
+import { RESET_CONSTRUCTOR } from './burger-constructor';
 import { SET_MODAL } from './modal'
 
 export const GET_ORDER_REQUEST = 'GET_ORDER_REQUEST';
 export const GET_ORDER_SUCCESS = 'GET_ORDER_SUCCESS';
 export const GET_ORDER_FAILED = 'GET_ORDER_FAILED';
 
-export const renderOrder = (orderId) => async dispatch => {
+export const renderOrder = (orderId, setLoading) => async dispatch => {
     dispatch({ type: GET_ORDER_REQUEST });
+    setLoading(true);
     postOrder(orderId)
         .then(res => {
             dispatch({ type: GET_ORDER_SUCCESS, payload: res });
             dispatch({ type: SET_MODAL, payload: res, content: 'order' });
+            dispatch({type: RESET_CONSTRUCTOR});
         })
-        .catch(() => dispatch({ type: GET_ORDER_FAILED }));
+        .catch(() => dispatch({ type: GET_ORDER_FAILED }))
+        .finally(() => setLoading(false));
 }
